893088a0a4ad5b35e8fa57d627137db78afb5393
rjcf18 <ricardo.fusco2@gmail.com>
2014-12-25 01:37:42 +0000

    update PD

--- Programacao Declarativa/trabalho_PD/linetris.pl
+++ Programacao Declarativa/trabalho_PD/linetris.pl
@@ -17,16 +17,16 @@ cria_linhas(Ncolunas,[L|Ls]) :-
 /* instancia a variável na posição(X,Y) com um elemento E, ou, caso lá esteja um
   elemento atómico, devolve o elemento E que se encontra nessa posição */
 posicao(X,Y,E,Matriz):-
-	nth1(X,Matriz,LinhaX),
-	nth1(Y,LinhaX,E).
+	 nth1(X,Matriz,LinhaX),
+   nth1(Y,LinhaX,E).
 
 /* faz um findall de todos os valores não atómicos da coluna pretendida para uma
   lista e o tamanho dessa lista irá ser a linha exacta onde instanciar com a
   peca a inserir. Caso já todos os elementos dessa coluna estejam instanciados,
   ou seja, caso sejam todos atómicos, o predicado falha.*/
-insere(Peca,Coluna,Matriz):-
-	findall(E,(posicao(_,Coluna,E,Matriz), \+ atomic(E)),L), length(L,N),
-  (\+ all_atomic(L),posicao(N,Coluna,Peca,Matriz));(fail).
+insere(Peca,Coluna,(Matriz,p(Linha,Coluna))):-
+	findall(E,(posicao(_,Coluna,E,Matriz), \+ atomic(E)),L), length(L,Linha),
+  (\+ all_atomic(L),posicao(Linha,Coluna,Peca,Matriz));(fail).
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 %loop menu PRINCIPAL
@@ -48,7 +48,9 @@ menuOpt(1):-
       write('N? \n'), read(Nlinhas), write(Nlinhas),
   write('\n\nM? \n'), read(Ncolunas), write(Ncolunas), nl,
   ((integer(Nlinhas),integer(Ncolunas), Nlinhas > 3,Ncolunas > 3,
-     inicializar(Nlinhas,Ncolunas,Matriz), ciclo(1,Matriz,hvsh));menuOpt(1)).
+     inicializar(Nlinhas,Ncolunas,Matriz), ciclo(1,(Matriz,p(1,1)),hvsh)
+    );menuOpt(1)
+  ).
 
 %caso de escolha da opção 2 (Humano vs AI)
 menuOpt(2):-
@@ -71,65 +73,72 @@ subMenu(N,M):-
     write('     1 - Humano\n'),
     write('     2 - AI\n\n'),
     write('Insira o número correspondente à opção:\n'), read(J),write(J),nl,
-  (integer(J),between(1,2,J), inicializar(N,M,Matriz),ciclo(J,Matriz,hvsc));
+  (integer(J),between(1,2,J), inicializar(N,M,Matriz),ciclo(J,(Matriz,p(1,1)),hvsc));
    (subMenu(N,M)).
 
- /* ciclos modo humano vs humano e humano vs AI. Jogador 1 é sempre as peças pretas
- e o jogador 2 e AI são sempre as peças brancas */
+/* predicado para controlar qual o jogador 1 e jogador 2 durante o ciclo */
+player(1,'Jogador 1',_).
+player(2,'Jogador 2',hvsh).
+player(2,'AI',hvsc).
 
- % primeira coisa no ciclo é verificar o terminal do jogo (se alguém ganhou)
- ciclo(_,Matriz,Modo):-
-   verifica_terminal(Matriz), !, print_tabuleiro(Matriz),
-   ((Modo = hvsh,write('O vencedor é o jogador: '), ganhou(J), write(J));
-   (Modo = hvsc, write('O vencedor é: '), ganhou(X), player_AI(X,J),write(J))).
 
- /* caso não haja condições de vitória a 2a verificação a fazer é se a última
- linha do tabuleiro se encontra preenchida, caso esteja é necessário
- eliminá-la e descer as restantes linhas para baixo*/
- ciclo(Jogador,Matriz,Modo):-
-   length(Matriz,Nlinhas),nth1(Nlinhas,Matriz,UltimaLinha),
-   all_atomic(UltimaLinha), desce_tabuleiro(Matriz,Nlinhas,NovaMatriz),
-   ciclo(Jogador,NovaMatriz,Modo).
+/* ciclos modo humano vs humano e humano vs AI. Jogador 1 é sempre as peças pretas
+  e o jogador 2 e AI são sempre as peças vermelhas */
 
- % jogador 1 a jogar, caso a inserção falhe é pedido input novamente ao jogador 1
- ciclo(1,Matriz,Modo):-
-   print_tabuleiro(Matriz),
-   write('Jogador 1 insira a coluna onde jogar: \n'),
-   read(Coluna), write(Coluna), nl,
-   (insere(p,Coluna,Matriz), ciclo(2,Matriz,Modo));(ciclo(1,Matriz,Modo)).
+% primeira coisa no ciclo é verificar o terminal do jogo (se alguém ganhou)
+ciclo(_,(Matriz,UltPecaJog),Modo):-
+  verifica_terminal(Matriz,UltPecaJog), !, print_tabuleiro(Matriz),
+  write('O vencedor é: '), ganhou(X), player(X,J,Modo),write(J).
 
- /* jogador 2 a jogar (modo humano vs humano), caso a inserção falhe é pedido
- input novamente ao jogador 2*/
- ciclo(2,Matriz,hvsh):-
-   print_tabuleiro(Matriz),
-   write('Jogador 2 insira a coluna onde jogar: \n'),
-   read(Coluna), write(Coluna),nl,
-   (insere(b,Coluna,Matriz), ciclo(1,Matriz,hvsh));(ciclo(2,Matriz,hvsh)).
+/* caso não haja condições de vitória a 2a verificação a fazer é se a última
+  linha do tabuleiro se encontra preenchida, caso esteja é necessário
+  eliminá-la e descer as restantes linhas para baixo*/
+ciclo(Jogador,(Matriz,UltPecaJog),Modo):-
+  length(Matriz,Nlinhas),nth1(Nlinhas,Matriz,UltimaLinha),
+  all_atomic(UltimaLinha), desce_tabuleiro(Matriz,Nlinhas,NovaMatriz),
+  ciclo(Jogador,(NovaMatriz,UltPecaJog),Modo).
 
- /* jogador 2 (AI) a jogar (modo humano vs AI). São encontrados todos os pares
- Valor-Coluna cujo valor é determinado usando uma função de avaliação (heurística),
- e é seleccionado o par com o maior valor da lista resultante sendo o segundo
- valor do par a coluna onde a peça irá ser jogada pela AI*/
- ciclo(2,Matriz,hvsc):-
-   print_tabuleiro(Matriz),
-   write('AI a calcular jogada... \n'),
-   nth1(1,Matriz,Linha),
-   length(Linha, Ncolunas),
-   geraNumsI_J(1,Ncolunas,Colunas),
-   findall(Valor-Coluna2, ( member(Coluna1,Colunas),insere(b,Coluna1,Matriz),
-                            func_aval((Matriz,b),Valor1),
-                            member(Coluna2,Colunas),insere(p,Coluna2,Matriz),
-                            func_aval((Matriz,b),Valor2),Valor is Valor1+Valor2
-                          ), Lista),
-   max_member(_-Coluna,Lista),
-   insere(b,Coluna,Matriz),
-   write('\nAI a jogar na coluna: '),
-   write(Coluna),nl,
-   ciclo(1,Matriz,hvsc).
+% jogador 1 a jogar, caso a inserção falhe é pedido input novamente ao jogador 1
+ciclo(1,(Matriz,UltPecaJog),Modo):-
+  print_tabuleiro(Matriz),
+  write('Jogador 1 insira a coluna onde jogar: \n'),
+  read(Coluna), write(Coluna), nl,
+  (insere(p,Coluna,(Matriz,UltPecaJog1)),ciclo(2,(Matriz,UltPecaJog1),Modo));
+   ciclo(1,(Matriz,UltPecaJog),Modo).
+
+/* jogador 2 a jogar (modo humano vs humano), caso a inserção falhe é pedido
+  input novamente ao jogador 2*/
+ciclo(2,(Matriz,UltPecaJog),hvsh):-
+  print_tabuleiro(Matriz),
+  write('Jogador 2 insira a coluna onde jogar: \n'),
+  read(Coluna), write(Coluna),nl,
+  (insere(v,Coluna,(Matriz,UltPecaJog2)),ciclo(1,(Matriz,UltPecaJog2),Modo));
+   ciclo(2,(Matriz,UltPecaJog),Modo).
+
+/* jogador 2 (AI) a jogar (modo humano vs AI). São encontrados todos os pares
+  Valor-Coluna cujo valor é determinado usando uma função de avaliação (heurística),
+  e é seleccionado o par com o maior valor da lista resultante sendo o segundo
+  valor do par a coluna onde a peça irá ser jogada pela AI*/
+ciclo(2,(Matriz,_),hvsc):-
+  print_tabuleiro(Matriz),
+  write('AI a calcular jogada... \n'),
+  nth1(1,Matriz,Linha),
+  length(Linha, Ncolunas),
+  geraNumsI_J(1,Ncolunas,Colunas),
+  findall(Valor-Coluna2, ( member(Coluna1,Colunas),insere(v,Coluna1,(Matriz,_)),
+                          func_aval((Matriz,v),Valor1),
+                          member(Coluna2,Colunas),insere(p,Coluna2,(Matriz,_)),
+                          func_aval((Matriz,v),Valor2),Valor is Valor1+Valor2
+                        ), Lista ),
+  max_member(_-Coluna,Lista),
+  insere(v,Coluna,(Matriz,UltPecaJog)),
+  write('\nAI a jogar na coluna: '),
+  write(Coluna),nl,
+  ciclo(1,(Matriz,UltPecaJog),hvsc).
 
 % gera uma lista com todos os numeros de I a J (inclusivé)
 geraNumsI_J(I,J,L):-
-   findall(N, between(I,J,N), L).
+  findall(N, between(I,J,N), L).
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
@@ -166,172 +175,184 @@ remove_nth(N,[Y|Xs],[Y|Ys]):- N > 1,
 desce_tabuleiro(Matriz,Ncolunas,Matriz3):-
   nth1(Ncolunas,Matriz,UltimaLinha), remove_nth(Ncolunas,Matriz,Matriz2),
   length(UltimaLinha,N1), length(NovaLinha,N1),
-  append([NovaLinha],Matriz2,Matriz3),!.
-
-/* predicado para controlar qual o jogador 1 e jogador 2 durante o
-  ciclo do modo humano vs AI*/
-player_AI(1,'Jogador 1').
-player_AI(2,'AI').
+  append([NovaLinha],Matriz2,Matriz3).
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
 %guarda quem ganhou o jogo
 guarda(ganhou(P)):- retractall(ganhou(_)), asserta(ganhou(P)).
 
-% conta o número de peças brancas ou peças pretas numa lista e guarda quem
+% conta o número de peças vermelhas ou peças pretas numa lista e guarda quem
 % ganhou se algum dos jogadores tiver 4 peças seguidas, caso chegue ao fim da
 % lista e nao haja 4 pecas seguidas, o predicado falha
 ver_pecas(_,4,_):- guarda(ganhou(1)),!.
 ver_pecas(_,_,4):- guarda(ganhou(2)),!.
-ver_pecas([],Np,Nb):- Np < 4, Nb < 4, fail.
+ver_pecas([],Np,Nv):- Np < 4, Nv < 4, fail.
 
 ver_pecas([E|Es],_,_):- \+ atomic(E),ver_pecas(Es,0,0).
 
-ver_pecas([E|Es],Np,Nb):-
-  atomic(E), (E == p, Np1 is Np + 1,ver_pecas(Es,Np1,Nb));
-             (E == b, Nb1 is Nb + 1,ver_pecas(Es,Np,Nb1)).
+ver_pecas([E|Es],Np,Nv):-
+  atomic(E), (E == p, Np1 is Np + 1,ver_pecas(Es,Np1,0));
+             (E == v, Nv1 is Nv + 1,ver_pecas(Es,0,Nv1)).
 
-% verifica as linhas, colunas e diagonais. predicado falha se nenhum dos jogadores
-% estiver em condições de vitória
-verifica_terminal(Matriz):-
-	verifica_linhas(Matriz);
-  (nth1(1,Matriz,Linha), length(Linha,Ncolunas),
-    verifica_colunas(Matriz,Ncolunas) );
-  (verifica_diagonais(Matriz) ).
+/* verifica a linha e coluna onde a ultima peca foi jogada e verifica as
+  diagonais no tabuleiro. Recebe como arg o tabuleiro e as coordenadas onde a
+  ultima peca foi jogada (p(X,Y)). O predicado falha se nenhum dos jogadores
+  estiver em condições de vitória */
+verifica_terminal(Matriz,UltPecaJog):-
+	verifica_linha(Matriz,UltPecaJog);
+  verifica_coluna(Matriz,UltPecaJog);
+  verifica_diagonais(Matriz).
 
-verifica_linhas([]):-!,fail.
-verifica_linhas([L|Ls]):-
-  ver_pecas(L,0,0);verifica_linhas(Ls).
+% verifica a linha onde a peca foi inserida
+verifica_linha(Matriz,p(X,_)):-
+  (nth1(X,Matriz,Linha), ver_pecas(Linha,0,0)).
 
-verifica_colunas(Matriz,N):-
-  N > 0, findall(E, posicao(_,N,E,Matriz),L),
-  ((ver_pecas(L,0,0),!); (N2 is N-1,verifica_colunas(Matriz,N2))).
+% verifica a coluna onde a peca foi inserida
+verifica_coluna(Matriz,p(_,Y)):-
+  (findall(E, posicao(_,Y,E,Matriz),L),ver_pecas(L,0,0)).
 
-/* verifica todas as diagonais para o jogador 1 e jogador 2*/
+/* verifica se há diagonais com 4 pecas seguidas*/
 verifica_diagonais(Matriz):-
   (check_diags(Matriz,p),guarda(ganhou(1)));
-  (check_diags(Matriz,b),guarda(ganhou(2))).
+  (check_diags(Matriz,v),guarda(ganhou(2))).
 
-check_diags(M,J):-
-  findall((p(X,Y),E), (posicao(X,Y,E,M),atomic(E)), L),
-  findall(J, encontra(L,J), L2),length(L2, V), V > 0.
+% procura todas as diagonais com 4 pecas seguidas e caso haja 1 ou mais,
+% significa que um dos jogadores ganhou
+check_diags(Matriz,Peca):-
+  findall((p(X,Y),E), (posicao(X,Y,E,Matriz),atomic(E)), L),
+  findall(Peca, encontra(L,Peca), L2),length(L2, V), V > 0.
 
-encontra(L,J):-
-  member((p(X,Y), J), L),
+% procura diagonal do canto inferior esquerdo para o canto
+% superior direito que tenha 4 pecas seguidas
+encontra(L,Peca):-
+  member((p(X,Y), Peca), L),
   X1 is X+1, Y1 is Y+1, X2 is X+2, Y2 is Y+2, X3 is X+3, Y3 is Y+3,
-  member((p(X1,Y1),J), L), member((p(X2,Y2),J), L), member((p(X3,Y3),J), L).
+  member((p(X1,Y1),Peca), L), member((p(X2,Y2),Peca), L), member((p(X3,Y3),Peca), L).
 
-encontra(L,J):-
-  member((p(X,Y), J), L),
+% encontra diagonal do canto superior esquerdo para o canto
+% inferior direito que tenha 4 pecas seguidas
+encontra(L,Peca):-
+  member((p(X,Y), Peca), L),
   X1 is X+1, X2 is X+2, X3 is X+3, Y1 is Y-1, Y2 is Y-2, Y3 is Y-3,
-  member((p(X1,Y1),J), L), member((p(X2,Y2),J), L), member((p(X3,Y3),J), L).
+  member((p(X1,Y1),Peca), L), member((p(X2,Y2),Peca), L), member((p(X3,Y3),Peca), L).
 
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
-% conta o numero total de 1p's ou 1b's
-find_all_1peca(M, J, V):-
+% conta o numero total de 1p's ou 1v's
+find_all_1peca(Matriz, Peca, N):-
   %encontra todos os membros atomicos do estado
-  findall((p(X,Y),E), (posicao(X,Y,E,M),atomic(E)), L),
-  findall(J, find_1peca(L, J), L2),length(L2, V).
+  findall((p(X,Y),E), (posicao(X,Y,E,Matriz),atomic(E)), L),
+  findall(Peca, find_1peca(L, Peca), L2),length(L2, N).
 
-% encontrar num. de 1p's ou 1b's
-find_1peca(L, J):-
-  member((p(X,Y), J), L),
+% encontrar 1p's ou 1v's
+find_1peca(L, Peca):-
+  member((p(X,Y), Peca), L),
   X1 is X+1, X2 is X-1,Y1 is Y-1, Y2 is Y+1,
-  \+ member((p(X,Y1),J), L), \+ member((p(X,Y2),J), L), \+ member((p(X1,Y),J), L),
-  \+ member((p(X2,Y),J), L), \+ member((p(X1,Y1),J), L), \+ member((p(X2,Y2),J), L),
-  \+ member((p(X1,Y2),J), L), \+ member((p(X2,Y1),J), L).
+  \+ member((p(X,Y1),Peca), L), \+ member((p(X,Y2),Peca), L),
+  \+ member((p(X1,Y),Peca), L), \+ member((p(X2,Y),Peca), L),
+  \+ member((p(X1,Y1),Peca), L), \+ member((p(X2,Y2),Peca), L),
+  \+ member((p(X1,Y2),Peca), L), \+ member((p(X2,Y1),Peca), L).
 
-% conta o numero total de 2p's ou 2b's
-find_all_2pecas(M, J, V):-
-  findall((p(X,Y),E), (posicao(X,Y,E,M),atomic(E)), L),
-  findall(J, find_2pecas(L, J), L2),length(L2, V).
+% conta o numero total de 2p's ou 2v's
+find_all_2pecas(Matriz, Peca, N):-
+  findall((p(X,Y),E), (posicao(X,Y,E,Matriz),atomic(E)), L),
+  findall(Peca, find_2pecas(L, Peca), L2),length(L2, N).
 
-% encontrar num. de 2p's ou 2b's na linha
-find_2pecas(L, J):-
-  member((p(X,Y), J), L),
+% encontrar 2p's ou 2v's na linha
+find_2pecas(L, Peca):-
+  member((p(X,Y), Peca), L),
   Y1 is Y-1,Y2 is Y-2,
-  member((p(X,Y1),J), L), member((p(X,Y2),J), L),(\+ atomic(J1); \+ J1=J).
+  member((p(X,Y1),Peca), L), member((p(X,Y2),Peca), L),
+  (\+ atomic(Peca1); \+ Peca1=Peca).
 
-% encontrar num. de 2p's ou 2b's na coluna
-find_2pecas(L, J):-
-  member((p(X,Y), J), L),
+% encontrar 2p's ou 2v's na coluna
+find_2pecas(L, Peca):-
+  member((p(X,Y), Peca), L),
   X1 is X-1, X2 is X-2,
-  member((p(X1,Y),J), L),member((p(X2,Y),J1), L),(\+ atomic(J1); \+ J1=J).
+  member((p(X1,Y),Peca), L), member((p(X2,Y),Peca1), L),
+  (\+ atomic(Peca1); \+ Peca1=Peca).
 
 %diagonal 1
-find_2pecas(L, J):-
-  member((p(X,Y), J), L),
+find_2pecas(L, Peca):-
+  member((p(X,Y), Peca), L),
   X1 is X-1, Y1 is Y-1,X2 is X-2, Y2 is Y-2,
-  member((p(X1,Y1),J), L),member((p(X2,Y2),J1), L),(\+ atomic(J1); \+ J1=J).
+  member((p(X1,Y1),Peca), L), member((p(X2,Y2),Peca1), L),
+  (\+ atomic(Peca1); \+ Peca1=Peca).
 
 %diagonal 2
-find_2pecas(L, J):-
-  member((p(X,Y), J), L),
+find_2pecas(L, Peca):-
+  member((p(X,Y), Peca), L),
   X1 is X-1, Y1 is Y+1,X2 is X-2,Y2 is Y-2,
-  member((p(X1,Y1),J), L),member((p(X2,Y2),J1), L),(\+ atomic(J1); \+ J1=J).
+  member((p(X1,Y1),Peca), L), member((p(X2,Y2),Peca1), L),
+  (\+ atomic(Peca1); \+ Peca1=Peca).
 
-% conta o numero total de 3p's ou 3b's
-find_all_3pecas(M, J, V):-
-  findall((p(X,Y),E), (posicao(X,Y,E,M),atomic(E)), L),
-  findall(J, find_3pecas(L, J), L2),length(L2, V).
+% conta o numero total de 3p's ou 3v's
+find_all_3pecas(Matriz, Peca, N):-
+  findall((p(X,Y),E), (posicao(X,Y,E,Matriz),atomic(E)), L),
+  findall(Peca, find_3pecas(L, Peca), L2),length(L2, N).
 
-% encontrar num. de 3p's ou 3b's na linha
-find_3pecas(L, J):-
-  member((p(X,Y), J), L),
-  Y1 is Y+1, Y2 is Y+2, Y3 is Y+3, member((p(X,Y1),J), L),
-  member((p(X,Y2),J), L), member((p(X,Y3),J1), L), (\+ atomic(J1); \+ J1=J).
+% encontrar 3p's ou 3v's na linha
+find_3pecas(L, Peca):-
+  member((p(X,Y), Peca), L),
+  Y1 is Y+1, Y2 is Y+2, Y3 is Y+3, member((p(X,Y1),Peca), L),
+  member((p(X,Y2),Peca), L), member((p(X,Y3),Peca1), L),
+  (\+ atomic(Peca1); \+ Peca1=Peca).
 
-% encontrar num. de 3p's ou 3b's na coluna
-find_3pecas(L, J):-
-  member((p(X,Y), J), L), X1 is X+1, X2 is X+2,X3 is X+3,member((p(X1,Y),J), L),
-  member((p(X2,Y),J), L), member((p(X3,Y),J1), L),(\+ atomic(J1); \+ J1=J).
+% encontrar 3p's ou 3v's na coluna
+find_3pecas(L, Peca):-
+  member((p(X,Y), Peca), L), X1 is X+1, X2 is X+2,X3 is X+3,
+  member((p(X1,Y),Peca), L),member((p(X2,Y),Peca), L),member((p(X3,Y),Peca1), L),
+  (\+ atomic(Peca1); \+ Peca1=Peca).
 
 %diagonal 1
-find_3pecas(L, J):-
-  member((p(X,Y), J), L),
-  X1 is X+1, Y1 is Y+1, X2 is X+2, Y2 is Y+2,X3 is X+3,
-  Y3 is Y+3,member((p(X1,Y1),J), L), member((p(X2,Y2),J), L),
-  member((p(X3,Y3),J1), L),(\+ atomic(J1); \+ J1=J).
+find_3pecas(L, Peca):-
+  member((p(X,Y), Peca), L),
+  X1 is X+1, Y1 is Y+1, X2 is X+2, Y2 is Y+2,X3 is X+3, Y3 is Y+3,
+  member((p(X1,Y1),Peca), L), member((p(X2,Y2),Peca), L),
+  member((p(X3,Y3),Peca1), L),(\+ atomic(Peca1); \+ Peca1=Peca).
 
 %diagonal 2
-find_3pecas(L, J):-
-  member((p(X,Y), J), L),
+find_3pecas(L, Peca):-
+  member((p(X,Y), Peca), L),
   X1 is X+1, X2 is X+2, Y1 is Y-1, Y2 is Y-2, X3 is X+3, Y3 is Y-3,
-  member((p(X1,Y1),J), L), member((p(X2,Y2),J), L),
-  member((p(X3,Y3),J1), L),(\+ atomic(J1); \+ J1=J).
+  member((p(X1,Y1),Peca), L), member((p(X2,Y2),Peca), L),
+  member((p(X3,Y3),Peca1), L),(\+ atomic(Peca1); \+ Peca1=Peca).
 
-% conta o numero total de 4p's ou 4b's
-find_all_4pecas(M, J, V):-
-  findall((p(X,Y),E), (posicao(X,Y,E,M),atomic(E)), L),
-  findall(J, find_4pecas(L, J), L2),length(L2, V).
+% conta o numero total de 4p's ou 4v's
+find_all_4pecas(Matriz, Peca, N):-
+  findall((p(X,Y),E), (posicao(X,Y,E,Matriz),atomic(E)), L),
+  findall(Peca, find_4pecas(L, Peca), L2),length(L2, N).
 
-% encontrar num. de 4p's ou 4b's na linha
-find_4pecas(L, J):-
-  member((p(X,Y), J), L),
-  Y1 is Y+1, Y2 is Y+2, Y3 is Y+3,member((p(X,Y1),J), L),
-  member((p(X,Y2),J), L), member((p(X,Y3),J), L).
+% encontrar 4p's ou 4v's na linha
+find_4pecas(L, Peca):-
+  member((p(X,Y), Peca), L),
+  Y1 is Y+1, Y2 is Y+2, Y3 is Y+3,
+  member((p(X,Y1),Peca), L),
+  member((p(X,Y2),Peca), L),
+  member((p(X,Y3),Peca), L).
 
-% encontrar num. de 4p's ou 4b's na coluna
-find_4pecas(L, J):-
-  member((p(X,Y), J), L),
-  X1 is X+1, X2 is X+2,X3 is X+3,member((p(X1,Y),J), L),
-  member((p(X2,Y),J), L),member((p(X3,Y),J), L).
+% encontrar 4p's ou 4v's na coluna
+find_4pecas(L, Peca):-
+  member((p(X,Y), Peca), L),
+  X1 is X+1, X2 is X+2,X3 is X+3,
+  member((p(X1,Y),Peca), L), member((p(X2,Y),Peca), L),
+  member((p(X3,Y),Peca), L).
 
 %diagonal 1
-find_4pecas(L, J):-
-  member((p(X,Y), J), L),
+find_4pecas(L, Peca):-
+  member((p(X,Y), Peca), L),
   X1 is X+1, Y1 is Y+1, X2 is X+2, Y2 is Y+2, X3 is X+3, Y3 is Y+3,
-  member((p(X1,Y1),J), L), member((p(X2,Y2),J), L),
-  member((p(X3,Y3),J), L).
+  member((p(X1,Y1),Peca), L), member((p(X2,Y2),Peca), L),
+  member((p(X3,Y3),Peca), L).
 
 %diagonal 2
-find_4pecas(L, J):-
-  member((p(X,Y), J), L),
+find_4pecas(L, Peca):-
+  member((p(X,Y), Peca), L),
   X1 is X+1, X2 is X+2, X3 is X+3, Y1 is Y-1, Y2 is Y-2, Y3 is Y-3,
-  member((p(X1,Y1),J), L), member((p(X2,Y2),J), L),
-  member((p(X3,Y3),J), L).
+  member((p(X1,Y1),Peca), L), member((p(X2,Y2),Peca), L),
+  member((p(X3,Y3),Peca), L).
 
 % avalia(Estado, Avaliacao)
 % dados a matriz(M) e a peça(P) que vai jogar, returna em Val o valor da avaliacao
@@ -348,5 +369,5 @@ func_aval((Matriz,Peca), Val):-
   Val1 is (V1+3*V2+V3+9*V7)-(V4+3*V5+6*V6+9*V8),
   ((V8 >= 1,Val is Val1+1000);(V7 >= 1, Val is Val1+800);Val is Val1) .
 
-inverteJog(b,p).
-inverteJog(p,b).
+inverteJog(v,p).
+inverteJog(p,v).
